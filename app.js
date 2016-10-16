8ec423564a083f44997389a378d88bb035c76cb2
Cliff Anderson <canderson@onecloudlabs.com>
2016-10-16 16:40:00 -0400

    Basic site and stylus template

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 /*
- *
+ * Dependencies
  */
 
 var express = require('express')
   , stylus = require('stylus')
-  , nib = require('nim');
+  , nib = require('nib');
 
 var app = express();
 function compile(str, path) {
 	return stylus(str).set('filename', path).use(nib());
 }
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
+app.use(express.logger('dev'));
+app.use(stylus.middleware( {
+    src: __dirname + '/public',
+    compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index', { title: 'Home', menu_items: ['test1', 'test2'] });
+});
+
+app.listen(3000);
